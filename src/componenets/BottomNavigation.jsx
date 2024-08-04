c1f91b9cc9fc91307110f728952d84edd431dc89
wondmeneh123 <wondmenhfekadu@gmail.com>
2024-08-04 02:35:40 -0700

    bottom issue fixed

--- src/componenets/BottomNavigation.jsx
+++ src/componenets/BottomNavigation.jsx
@@ -18,32 +18,35 @@ const BottomNavigation = () => {
   const [cart, setCart] = useState([]);
 
   return (
-    <Router className="fixed">
-      <div className="app bg-[#F5F5F0]">
-        <Routes>
-          <Route path="/" element={<Shop cart={cart} setCart={setCart} />} />
-          <Route path="/add" element={<Add />} />
-          <Route path="/item/:id" element={<ItemDetail />} />
-          <Route path="/cart" element={<Cart cart={cart} />} />
-        </Routes>
-        <div className="links">
-          <NavLink to="/" end className="singleLink" activeClassName="active">
-            <BiHome className="icon" size={20} />
-            <p>Home</p>
-          </NavLink>
-          <NavLink to="/add" className="singleLink" activeClassName="active">
-            <FaPlusCircle className="icon" size={20} />
-            <p>Post Ad</p>
-          </NavLink>
-
-          <NavLink
-            to="/account"
-            className="singleLink"
-            activeClassName="active"
-          >
-            <IoPersonSharp className="icon" size={20} />
-            <p>Acccount</p>
-          </NavLink>
+    <Router>
+      <div className="bg-[#F5F5F0]">
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Shop cart={cart} setCart={setCart} />} />
+            <Route path="/add" element={<Add />} />
+            <Route path="/item/:id" element={<ItemDetail />} />
+            <Route path="/cart" element={<Cart cart={cart} />} />
+          </Routes>
+        </div>
+        <div className="fixed bottom-0 left-0 right-0 bg-[#d43790] shadow-lg border-t border-gray-200 z-50">
+          <div className="flex justify-around p-3">
+            <NavLink to="/" end className="singleLink" activeClassName="active">
+              <BiHome className="icon" size={20} />
+              <p>Home</p>
+            </NavLink>
+            <NavLink to="/add" className="singleLink" activeClassName="active">
+              <FaPlusCircle className="icon" size={20} />
+              <p>Post Ad</p>
+            </NavLink>
+            <NavLink
+              to="/account"
+              className="singleLink"
+              activeClassName="active"
+            >
+              <IoPersonSharp className="icon" size={20} />
+              <p>Account</p>
+            </NavLink>
+          </div>
         </div>
       </div>
     </Router>
